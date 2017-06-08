915781a0cd1d05682c3766d1798e1521473339a8
valdadyne <rodgers.gitau@gmail.com>
2017-06-08 13:29:47 +0300

    Login/Register

--- taskmgr/views.py
+++ taskmgr/views.py
@@ -1,7 +1,24 @@
-from flask import render_template, redirect
-from taskmgr import app
+from flask import render_template, request, redirect
+from taskmgr import app,db
+
+Users = db.table('usr')
+Tasks = db.table('tasks')
+SubTasks = db.table('sub_tasks')
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/users')
+def users():
+    return render_template('users.html')
+
+@app.route('/SignUp', methods=['POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        organization = request.form['organization']
+        email = request.form['email']
+        raw_pwd = request.form['password']
